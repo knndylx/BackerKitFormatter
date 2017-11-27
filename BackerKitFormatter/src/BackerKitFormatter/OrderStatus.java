564f4658aab1b883552c07024e6ee7ad2cbfd475
knndylx <30755842+knndylx@users.noreply.github.com>
2017-11-27 15:08:40 -0700

    Changed the names of the files and the project
    Changed the name to BackerKitFormatter. Next step is creating an orderlist class and moving all the functions from the main class to that to clean up the main class.

---
+++ BackerKitFormatter/src/BackerKitFormatter/OrderStatus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackerKitFormatter;
+
+/**
+ *
+ * @author akenn
+ */
+public enum OrderStatus {
+    paid, locked_down, survey_not_answered, open, errored;
+}
